4fed71d8d3609329cb22f41d48a5bba359a00675
CalebCintary <calebcintaryy@gmail.com>
2022-07-08 17:48:54 +0300

    Task2Impl.assignNumbers commented
    Signed-off-by: CalebCintary <calebcintaryy@gmail.com>

--- src/main/java/ru/tandemservice/test/task2/Task2Impl.java
+++ src/main/java/ru/tandemservice/test/task2/Task2Impl.java
@@ -21,50 +21,72 @@ public class Task2Impl implements IElementNumberAssigner {
         return INSTANCE;
     }
 
+    /**
+     * <p>Алгоритм исполняется за примерно линейное время.</p>
+     * <p>Использование операций {@link IElement#setupNumber(int)} стремится к n + k. Если исходные номера состоят
+     * из промежутка [0;n] то k -> 0. Если же промежуток [0;{2, 3}n] то (при n = 1000) k -> {200, 300}. При увеличении
+     * коэффициента при n k -> 0</p>
+     * <p>Исходя из всего выше сказанного алгоритм работает с наибольшей эффективностью когда исходный список состоит из
+     *  [0; n] или если представленные значения сильно отличаются от [0; n]</p>
+     *
+     * @param elements элементы, которым нужно выставить номера
+     */
     @Override
     public void assignNumbers(final List<IElement> elements) {
+        // Заранее вычисляется максимальный номер для замен
         AtomicInteger max = new AtomicInteger(elements.get(0).getNumber());
         elements.forEach(element -> max.set(Math.max(element.getNumber(), max.get())));
 
+        // offset для старта с не идущих по порядку элементов
         int offset = 0;
 
         do {
             int differenceIndex = offset;
-            while (elements.get(differenceIndex).getNumber() == differenceIndex) {
-                if (++differenceIndex >= elements.size()) {
+            while (elements.get(differenceIndex).getNumber() == differenceIndex) { // Поиск первого элемента идущего не
+                if (++differenceIndex >= elements.size()) {                        // по порядку
                     offset = differenceIndex - 1;
                     break;
                 }
             }
 
-            if (differenceIndex >= elements.size()) {
+            if (differenceIndex >= elements.size()) { // Если поиск указывает на последний или дальше, то завершаем
                 break;
             }
 
-            int rabbitPointer = elements.get(differenceIndex).getNumber();
+            int rabbitPointer = elements.get(differenceIndex).getNumber(); // Прыгающий указатель для замен.
+            // Нестандартная ситуация. Оказалось, что значение в ячейке не входит в размер списка.
             if (rabbitPointer >= elements.size()) {
+                // Ищем такую ячейку, что она хранит значение, которое должно быть тут.
                 boolean changed = false;
                 for (int i = 0; i < elements.size(); ++i) {
+                    // Если такая ячейка нашлась, то в одной из следующих строк, происходит замена в этой точке
+                    // на номер, которого точно нет в списке, а rabbitPointer, указываем на точку различия с
+                    // нестандартной ситуацией, что закрывает её в последствии в основном проходе, и быстро выкидывает
+                    // на новую итерацию.
                     if (elements.get(i).getNumber() == differenceIndex) {
-                        rabbitPointer = i;
+                        rabbitPointer = differenceIndex;
+                        differenceIndex = i;
                         changed = true;
                         break;
                     }
                 }
-                if (!changed) {
+                if (!changed) { // Если такой ячейки не нашлось, то просто заменяем на нужное значение
                     elements.get(differenceIndex).setupNumber(differenceIndex);
                     continue;
-                } else {
-                    int temp = differenceIndex;
-                    differenceIndex = rabbitPointer;
-                    rabbitPointer = temp;
                 }
             }
 
+            // В первой точке различия ставим номер, которого гарантированно нет в списке.
             elements.get(differenceIndex).setupNumber(max.incrementAndGet());
             while (true) {
+                // Запоминаем значение в следующей ячейке
                 int nextRabbitPointer = elements.get(rabbitPointer).getNumber();
+                // В лучшем случае, rabbitPointer указывает на ячейку, с номером, которых хранился в differentIndex
+                // ячейке. Соответственно, так как его там уже нет, смело ставим в rabbitPointer ячейку правильное
+                // значение.
                 elements.get(rabbitPointer).setupNumber(rabbitPointer);
+                // Если следующее значение указателя, больше чем возможно, то выходим с цикла и ищем новое различие
+                // иначе прыгаем в следующую ячейку и повторяем операцию
                 if (nextRabbitPointer >= elements.size()) {
                     break;
                 } else {
